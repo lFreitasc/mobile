8b7552e1f12cacceed7c04fae86f4559d5e35a0b
Lucas Freitas <lucas.freitascosta96@gmail.com>
2019-05-07 01:20:00 -0300

    recycleview listener

--- app/src/main/java/br/com/freator/pegaeu/Activities/CrudActivity.java
+++ app/src/main/java/br/com/freator/pegaeu/Activities/CrudActivity.java
@@ -3,10 +3,16 @@ package br.com.freator.pegaeu.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import br.com.freator.pegaeu.Adapters.Adapter;
+import br.com.freator.pegaeu.Helpers.RecyclerItemClickListener;
 import br.com.freator.pegaeu.Helpers.fakeDataBase;
 import br.com.freator.pegaeu.R;
 
@@ -35,6 +41,30 @@ public class CrudActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         recyclerView.setAdapter(adapter);
+
+        recyclerView.addOnItemTouchListener(
+            new RecyclerItemClickListener(getApplicationContext(), recyclerView,
+                    new RecyclerItemClickListener.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(View view, int position) {
+//                            Toast.makeText(getApplicationContext(),db.getHeroes().get(position),Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                        @Override
+                        public void onLongItemClick(View view, int position) {
+
+                        }
+
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        }
+                    }
+            )
+        );
     }
 }
